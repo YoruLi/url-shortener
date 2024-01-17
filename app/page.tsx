5ed96cea2b53bf19ab737b529440be0e8d51e7ed
YoruLi <lionel3327@gmail.com>
2024-01-17 15:48:15 -0300

    feat Add LinkButtons animated

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
+"use client";
+
+import { UpTransition } from "@/components/motion";
+import { LinkButton } from "@/components/ui/link-button";
+
 export default function Home() {
   return (
     <div className="section">
-      <h1 className="text-3xl md:text-6xl mb-2 md:mb-5 font-semibold animate-pulse">
-        URL Shorterner
-      </h1>
+      <UpTransition>
+        <h1 className="text-4xl md:text-6xl mb-2 md:mb-5 font-semibold">URL Shorterner</h1>
+      </UpTransition>
+
+      <UpTransition delay={0.2}>
+        <h3 className="text-sm md:text-lg mb-2 md:mb-5 font-semibold  animate-pulse">
+          Create shorter URLS with Lym
+        </h3>
+      </UpTransition>
+
+      <UpTransition delay={0.4}>
+        <LinkButton href="/dashboard">Getting started</LinkButton>
+      </UpTransition>
     </div>
   );
 }
