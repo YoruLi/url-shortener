e12ba60e2db7df666658a7ba23c33b5c49b0019f
YoruLi <lionel3327@gmail.com>
2024-01-19 01:49:57 -0300

    feat(api/link/[slug]) Add GET method  for get single slug

--- app/api/link/[slug]/route.ts
+++ app/api/link/[slug]/route.ts
@@ -2,7 +2,37 @@ import { getServerSession } from "next-auth";
 import { options } from "../../auth/options";
 import { prisma } from "@/utils/db/client";
 import { NextResponse } from "next/server";
-import { revalidatePath, revalidateTag, unstable_noStore } from "next/cache";
+import { revalidateTag } from "next/cache";
+import { NextApiResponse } from "next";
+import { headers } from "next/headers";
+
+export const GET = async (
+  req: Request,
+
+  { params }: { params: { slug: string } }
+) => {
+  if (!params.slug) {
+    return NextResponse.json({ error: "Missing Slug..." }, { status: 400 });
+  }
+  try {
+    const slug = await prisma.link.findFirst({
+      where: {
+        slug: {
+          equals: params.slug,
+        },
+      },
+    });
+    console.log(slug);
+    if (!slug) {
+      return NextResponse.json({ error: "Slug not found" }, { status: 404 });
+    }
+    const headersList = headers();
+    headersList.set("Cache-Control", "s-maxage=1000000, stale-while-revalidate");
+    return NextResponse.json(slug, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: "Error getting slug" }, { status: 500 });
+  }
+};
 
 export const PUT = async (req: Request, { params }: { params: { slug: string } }) => {
   const body = await req.json();
