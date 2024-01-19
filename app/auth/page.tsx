cd47cbad12ba802542d79917d0956bf60d911ade
YoruLi <lionel3327@gmail.com>
2024-01-18 23:50:53 -0300

    feat: Add Auth Page

---
+++ app/auth/page.tsx
@@ -0,0 +1,22 @@
+import { Auth } from "@/components/auth";
+import { GitHub } from "@/components/icons/github";
+import { UpTransition } from "@/components/motion";
+
+import React from "react";
+
+export default function page() {
+  return (
+    <section className="section">
+      <UpTransition>
+        <div className="flex items-center gap-3 mb-2 md:mb-5">
+          <h1 className="text-4xl  md:text-6xl font-semibold capitalize">Sign in with</h1>
+          <GitHub className="size-20" />
+        </div>
+      </UpTransition>
+
+      <UpTransition delay={0.4}>
+        <Auth />
+      </UpTransition>
+    </section>
+  );
+}
