fa24aac0fdbfac8cd86ee114030eb73dcb223dd9
YoruLi <lionel3327@gmail.com>
2024-01-19 02:05:50 -0300

    ...

--- middleware.ts
+++ middleware.ts
@@ -25,7 +25,7 @@ export default withAuth(
     const data = await fetch(`${origin}/api/link/${slug}`);
 
     if (data.status == 404) {
-      return NextResponse.redirect(origin);
+      return NextResponse.redirect(req.nextUrl.origin);
     }
 
     const res = await data.json();
