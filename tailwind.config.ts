82e0486e5b438731404ce3dc43c66e44b35cba89
YoruLi <lionel3327@gmail.com>
2024-01-18 23:43:12 -0300

    feat(styles): Update some styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,9 +11,12 @@ const config: Config = {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-background":
+          "radial-gradient(circle, rgba(21, 21, 22, 0.9%) 0, hsl(0, 0%, 8.5%) 100%)",
       },
       colors: {
         primary: "hsl(var(--primary))",
+        background: "hsl(var(--background))",
       },
     },
   },
