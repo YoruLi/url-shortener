7e28acce14d1a86f8ed54ec159391c7d6ed48abe
YoruLi <lionel3327@gmail.com>
2024-01-19 19:20:56 -0300

    ...

--- app/api/url/[slug].ts
+++ app/api/url/[slug].ts
@@ -19,8 +19,8 @@ export const GET = async (req: CombineRequest, { params }: { params: { slug: str
         },
       },
     });
-    console.log(data);
-    if (!slug) {
+
+    if (!data) {
       return NextResponse.json({ error: "Slug not found" }, { status: 404 });
     }
 
@@ -28,30 +28,5 @@ export const GET = async (req: CombineRequest, { params }: { params: { slug: str
     return NextResponse.json(data, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error: "Error getting slug" }, { status: 500 });
-  }
-};
-
-export const PUT = async (req: Request, { params }: { params: { slug: string } }) => {
-  const body = await req.json();
-  const session = await auth();
-
-  if (!session?.user) {
-    return NextResponse.json({ message: "User not logged in." }, { status: 401 });
-  }
-  try {
-    const data = await prisma.link.update({
-      where: {
-        id: body.id,
-      },
-      data: {
-        ...body,
-        creatorId: session?.user?.id,
-      },
-    });
-    revalidateTag("links");
-    return NextResponse.json({ editedLink: data }, { status: 200 });
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json({ message: "Error creating a new link." }, { status: 500 });
   }
 };
--- middleware.ts
+++ middleware.ts
@@ -1,53 +1,3 @@
-// import { NextResponse } from "next/server";
-// import { withAuth, NextRequestWithAuth } from "next-auth/middleware";
-// import { protectedRoutes } from "../utils/protected-routes";
-
-// export default withAuth(
-//   async function middleware(req: NextRequestWithAuth) {
-//     const pathname = req.nextUrl?.pathname;
-//     const { origin } = new URL(req.url);
-//     const token = req.nextauth.token;
-
-//     const isAccessingSensitiveRoute = protectedRoutes.some((route) => pathname.startsWith(route));
-//     const isLoginPage = pathname.startsWith("/auth");
-
-//     if (isLoginPage) {
-//       if (!token && isAccessingSensitiveRoute) {
-//         return NextResponse.redirect(new URL("/auth", origin));
-//       }
-//     }
-
-//     const slug = req.nextUrl.pathname.split("/").pop();
-
-//     const data = await fetch(`${origin}/api/link?slug=${slug}`);
-
-//     if (data.status == 404) {
-//       return NextResponse.redirect(req.nextUrl.origin);
-//     }
-
-//     const res = await data.json();
-
-//     if (res.url) {
-//       return NextResponse.redirect(new URL(res.url, origin));
-//     }
-//   },
-//   {
-//     pages: {
-//       signIn: "/auth",
-//     },
-//     secret: "secret",
-//     callbacks: {
-//       authorized: ({ token }) => {
-//         return true;
-//       },
-//     },
-//   }
-// );
-
-// export const config = {
-//   matcher: ["/dashboard/", "/dashboard/:path*", "/:slug*"],
-// };
-
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 import { NextResponse } from "next/server";
@@ -60,40 +10,58 @@ export default auth(async (req) => {
   const { nextUrl } = req;
   const { origin } = new URL(req.url);
   const isLoggedIn = !!req.auth;
+  const pathname = req.nextUrl?.pathname;
   const session = await auth();
+  const isAccessingSensitiveRoute = ["/dashboard", "/dashboard/create"].some((route) =>
+    pathname.startsWith(route)
+  );
+  const isAuthRoute = "/auth";
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
 
-  // if (isApiAuthRoute) {
-  //   return null;
-  // }
-
-  // if (nextUrl.pathname === "/auth") {
+  if (isApiAuthRoute) {
+    return null;
+  }
+  // if (isAuthRoute) {
   //   if (isLoggedIn) {
-  //     return Response.redirect(new URL("/dashboard", nextUrl));
+  //     return Response.redirect(new URL("/", nextUrl));
   //   }
   //   return null;
   // }
 
   // if (!isLoggedIn) {
-  //   return Response.redirect(new URL(`/auth`, nextUrl));
+  //   if (!session?.user && isAccessingSensitiveRoute) {
+  //     return NextResponse.redirect(new URL("/auth", nextUrl));
+  //   }
   // }
 
-  // return null;
-
-  const pathname = req.nextUrl.pathname;
+  // if (nextUrl.pathname === "/auth") {
+  //   if (isLoggedIn && session?.user) {
+  //     return Response.redirect(new URL("/dashboard", nextUrl));
+  //   }
+  //   return null;
+  // }
 
   const parts = pathname.split("/");
   const shorUrl = parts[parts.length - 1];
-  const data = await fetch(`${origin}/api/link?slug=${shorUrl}`);
-  if (data.status === 200) {
-    const res = await data.json();
-    if (res.url) {
-      return NextResponse.redirect(new URL(res.url, origin));
+
+  try {
+    const data = await fetch(`http://localhost:3000/api/link?slug=${shorUrl}`, { method: "GET" });
+
+    if (data.status === 404) {
+      return NextResponse.redirect(req.nextUrl.origin);
     }
-  } else return NextResponse.redirect(req.nextUrl.origin);
+    const res = await data.json();
+    console.log(res);
+    if (data?.url) {
+      console.log(res.url);
+      return NextResponse.redirect(res.url);
+    }
+  } catch (error) {
+    console.error("Error parsing JSON response:", error);
+  }
 });
 
 export const config = {
-  matcher: ["/dashboard", "/:slug*"],
+  matcher: ["/go/:slug*"],
 };
