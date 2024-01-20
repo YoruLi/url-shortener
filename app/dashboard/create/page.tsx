933ce3a1066c4ae7d866d8395422d66497f89b37
YoruLi <lionel3327@gmail.com>
2024-01-19 23:13:56 -0300

    ...

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -1,8 +1,16 @@
+import { auth } from "@/auth";
 import LinkForm from "@/components/form/link";
+import { redirect } from "next/navigation";
 
 import React from "react";
 
-export default function CreateLinkPage() {
+export default async function CreateLinkPage() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/");
+  }
+
   return (
     <div className="container pt-8 mx-auto w-[90%]">
       <h1 className="title text-center mb-10">Create new link</h1>
