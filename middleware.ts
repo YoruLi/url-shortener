988f64c604b3c4e3e2b6f56303c1924eb98b271c
YoruLi <lionel3327@gmail.com>
2024-01-19 21:27:57 -0300

    feat(middleware): Add protecting routes

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 const { auth } = NextAuth(authConfig);
 
-export const apiAuthPrefix = "/auth";
+export const apiAuthPrefix = "/api/link";
 
 export default auth(async (req) => {
   const { nextUrl } = req;
@@ -12,6 +12,7 @@ export default auth(async (req) => {
   const isLoggedIn = !!req.auth;
   const pathname = req.nextUrl?.pathname;
   const session = await auth();
+
   const isAccessingSensitiveRoute = ["/dashboard", "/dashboard/create"].some((route) =>
     pathname.startsWith(route)
   );
@@ -20,47 +21,44 @@ export default auth(async (req) => {
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
 
   if (isApiAuthRoute) {
+    return Response.redirect(new URL("/", origin));
+  }
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL("/", nextUrl));
+    }
+  }
+
+  if (!session && isAccessingSensitiveRoute) {
+    return NextResponse.redirect(new URL("/auth", origin));
+  }
+
+  if (nextUrl.pathname === "/auth") {
+    if (isLoggedIn && session) {
+      return Response.redirect(new URL("/dashboard", nextUrl));
+    }
     return null;
   }
-  // if (isAuthRoute) {
-  //   if (isLoggedIn) {
-  //     return Response.redirect(new URL("/", nextUrl));
-  //   }
-  //   return null;
-  // }
-
-  // if (!isLoggedIn) {
-  //   if (!session?.user && isAccessingSensitiveRoute) {
-  //     return NextResponse.redirect(new URL("/auth", nextUrl));
-  //   }
-  // }
-
-  // if (nextUrl.pathname === "/auth") {
-  //   if (isLoggedIn && session?.user) {
-  //     return Response.redirect(new URL("/dashboard", nextUrl));
-  //   }
-  //   return null;
-  // }
 
   const parts = pathname.split("/");
   const shorUrl = parts[parts.length - 1];
   try {
-    const data = await fetch(`${origin}/api/link?slug=${shorUrl}`, { method: "GET" });
+    const data = await fetch(`${origin}/api/link/${shorUrl}`);
 
     if (data.status === 404) {
       return NextResponse.redirect(origin);
     }
     const res = await data.json();
-    console.log(res);
+
     if (data?.url) {
       console.log(res.url);
       return NextResponse.redirect(res.url);
     }
   } catch (error) {
-    console.error("Error parsing JSON response:", error);
+    console.error(error);
   }
 });
 
 export const config = {
-  matcher: ["/go/:slug*"],
+  matcher: ["/go/:slug*", "/api/:path*", "/dashboard/:path*"],
 };
