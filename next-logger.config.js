64f2479a54878347fd6b5287a82b66bff56dee90
YoruLi <lionel3327@gmail.com>
2024-01-20 01:36:56 -0300

    feat: Add next-logger

---
+++ next-logger.config.js
@@ -0,0 +1,12 @@
+const pino = require("pino");
+
+const logger = (defaultConfig) =>
+  pino({
+    ...defaultConfig,
+    messageKey: "message",
+    mixin: () => ({ name: "custom-pino-instance" }),
+  });
+
+module.exports = {
+  logger,
+};
