2ed1564939f75da137cb9cdbf3b716c8307f9f22
YoruLi <lionel3327@gmail.com>
2024-01-25 03:57:28 -0300

    ...

--- app/actions/index.ts
+++ app/actions/index.ts
@@ -83,13 +83,3 @@ export const getLinks = async (session: Session): Promise<link[]> => {
 
   return result;
 };
-
-export const getUserById = async (id: string) => {
-  try {
-    const user = await prisma.user.findUnique({ where: { id } });
-
-    return user;
-  } catch {
-    return null;
-  }
-};
